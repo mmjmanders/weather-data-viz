bd8f4c251345edaff2a35879f011ff7c461c0c03
Mark Manders <mmjmanders@gmail.com>
2026-01-09 17:08:28 +0100

    Add dark mode support and theme-based color palette (#4)
    - Introduced dark mode styling to `<body>` and `App.vue` components.
    - Added a thematic color palette (`Midnight`, `Cyan`, `Sunlight`, `Coral`) defined in `main.css`.

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
+  <h1 class="text-neutral-950 dark:text-neutral-50">You did it!</h1>
+  <p class="text-neutral-950 dark:text-neutral-50">
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
