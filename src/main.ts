42eaf3fc247fa5dca586290936518c870cb233da
Mark Manders <mmjmanders@gmail.com>
2026-01-09 15:13:25 +0100

    Integrate Vue Query Devtools
    - Add `VueQueryPlugin` with custom query client configuration to `main.ts`.
    - Include `VueQueryDevtools` in `App.vue` template for debugging.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(VueQueryPlugin, {
+    queryClientConfig: {
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 60 * 1000,
+        },
+      },
+    },
+  })
+  .mount('#app')
