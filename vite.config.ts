02db7528d535de4dd7af3f1f30c35cc47cf9dbb2
Mark Manders <mmjmanders@gmail.com>
2026-01-09 12:04:39 +0100

    Add Vite PWA plugin and update .gitignore for PWA files

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: 'autoUpdate',
+    }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
