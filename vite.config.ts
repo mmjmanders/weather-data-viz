6560500dc95daa4fedfb7e729b6d011ac61a2477
Mark Manders <mmjmanders@gmail.com>
2026-01-09 13:34:36 +0100

    Add pwa capabilities (#2)
    * Update dependencies and add plugins for PWA support
    
    * Add Vite PWA plugin and update .gitignore for PWA files
    
    * Add logo.svg to public directory
    
    * Add PWA icons to public directory
    
    * Update metadata and PWA manifest for "Weather Data Viz"

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,51 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Weather Data Viz',
+        short_name: 'WeatherViz',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
