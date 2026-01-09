d6f216d3866660021d4f2ad91571acc9a551710e
Mark Manders <mmjmanders@gmail.com>
2026-01-09 13:31:38 +0100

    Update metadata and PWA manifest for "Weather Data Viz"

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,34 @@ export default defineConfig({
         enabled: true,
       },
       registerType: 'autoUpdate',
+      manifest: {
+        name: 'Weather Data Viz',
+        short_name: 'WeatherViz',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
     }),
     vueDevTools(),
   ],
