d6cb7dfaa001ce0937a55449be944b407e34ecda
Mark Manders <mmjmanders@gmail.com>
2026-01-12 10:52:52 +0100

    Add `InputForm` component with Vee Validate and Zod integration

---
+++ src/components/InputForm.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useForm } from 'vee-validate'
+import { type InputForm as InputFormType, InputFormSchema } from '@/types'
+import { toTypedSchema } from '@vee-validate/zod'
+
+const emit = defineEmits<{
+  'submit:data': [startDate: string, endDate: string, location: string]
+}>()
+
+const { values, defineField } = useForm<InputFormType>({
+  validationSchema: toTypedSchema(InputFormSchema),
+  validateOnMount: false,
+})
+</script>
+
+<template></template>
+
+<style scoped></style>
