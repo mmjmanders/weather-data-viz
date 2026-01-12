5dcf1ff88bc7c3f12f9187509990f3a10fa03526
Mark Manders <mmjmanders@gmail.com>
2026-01-12 10:22:32 +0100

    Update neutral colors to slate in `<body>` and headings

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 </script>
 
 <template>
-  <h1 class="text-neutral-950 dark:text-neutral-50">You did it!</h1>
-  <p class="text-neutral-950 dark:text-neutral-50">
+  <h1 class="text-slate-950 dark:text-slate-50">You did it!</h1>
+  <p class="text-slate-950 dark:text-slate-50">
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
