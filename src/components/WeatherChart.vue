c80807c871d8182347dbb17859dcd681044b72d4
Mark Manders <mmjmanders@gmail.com>
2026-01-16 13:28:06 +0100

    Enhance WeatherChart with tooltips, legends, and responsive design
    - Add TooltipComponent and LegendComponent to improve chart interactivity.
    - Include axis label formatting using dayjs for better date display.
    - Adjust WeatherChart styles for responsiveness with TailwindCSS.

--- src/components/WeatherChart.vue
+++ src/components/WeatherChart.vue
@@ -3,29 +3,61 @@ import type { HistoricalWeather } from '@/types'
 import { use } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
 import { BarChart, LineChart } from 'echarts/charts'
-import { TitleComponent, GridComponent } from 'echarts/components'
+import {
+  TitleComponent,
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+} from 'echarts/components'
 import VChart from 'vue-echarts'
 import { computed } from 'vue'
 import { type EChartsOption } from 'echarts'
+import dayjs from 'dayjs'
 
 const props = defineProps<{
   weatherData: HistoricalWeather
 }>()
 
-use([CanvasRenderer, LineChart, BarChart, TitleComponent, GridComponent])
+use([
+  CanvasRenderer,
+  LineChart,
+  BarChart,
+  TitleComponent,
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+])
 
 const options = computed<EChartsOption>(() => ({
   title: {
     text: 'Weather data',
   },
+  legend: {
+    data: ['Temperature'],
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+    },
+  },
   xAxis: {
     type: 'time',
+    axisLabel: {
+      rotate: 45,
+      formatter: (val) => {
+        return dayjs(val).format('dddd, MMMM D, YYYY')
+      },
+    },
   },
   yAxis: [
     {
       id: 'Temperature',
       type: 'value',
       name: 'Temperature',
+      axisLabel: {
+        formatter: `{value} ${props.weatherData.daily_units.temperature_2m_mean}`,
+      },
     },
   ],
   series: [
@@ -43,13 +75,9 @@ const options = computed<EChartsOption>(() => ({
 </script>
 
 <template>
-  <VChart class="chart w-full" :option="options" />
+  <div class="min-h-96">
+    <VChart class="chart" :option="options" :autoresize="true" />
+  </div>
 </template>
 
-<style scoped>
-@reference "tailwindcss"
-
-.chart {
-  @apply h-96;
-}
-</style>
+<style scoped></style>
